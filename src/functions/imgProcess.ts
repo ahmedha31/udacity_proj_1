e6e7f8c57f39367bc875bd9065e3ec1db5dafb1a
ahmedha31 <aahhmmeeddhhaasseenn12332@gmail.com>
2022-11-22 03:31:29 +0200

    add funcions and middlewears

---
+++ src/functions/imgProcess.ts
@@ -0,0 +1,20 @@
+import fs from "fs";
+import sharp from "sharp";
+
+export default async function imgProcess(
+  image: string,
+  width: string,
+  height: string
+): Promise<string> {
+  var imgpath = `./images/${image}.jpg`;
+
+  return sharp(imgpath)
+    .resize(Number(width), Number(height))
+    .toFormat("jpg")
+    .toFile(`./tmp/${image}-${width}x${height}.jpg`).then((data)=>{
+        return `./tmp/${image}-${width}x${height}.jpg`;
+    }).catch((err)=>{
+      console.log(err);
+        throw err;
+    });
+}
---
+++ src/tests/function/imgProcess.test.ts
@@ -0,0 +1,20 @@
+import imgProcess from "../../functions/imgProcess";
+import fs from "fs";
+describe("Image Processing", () => {
+  it("should return a path to the resized image", async () => {
+    const image = "fjord";
+    const width = "200";
+    const height = "200";
+    try {
+      const data = await imgProcess(image, width, height);
+      console.log(data, "expected");
+      expect(data).toBe(`./tmp/${image}-${width}x${height}.jpg`);
+      fs.unlink(data, (err) => {
+        if (err) throw err;
+      });
+    } catch (err_1) {
+      console.log(err_1);
+      expect(err_1).toBeNull();
+    }
+  });
+});
