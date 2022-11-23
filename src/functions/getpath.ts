7e4e052b1832d4c4bd8040337b76a105f2f4fcaf
ahmedha31 <aahhmmeeddhhaasseenn12332@gmail.com>
2022-11-23 15:46:02 +0200

    fix bugs #3

---
+++ src/functions/getpath.ts
@@ -0,0 +1,9 @@
+import path from "path";
+
+export function imagespath() : string {
+  return path.join(process.cwd(), "images");
+}
+
+export function tmppath(): string {
+  return path.join(process.cwd(), "tmp");
+}
